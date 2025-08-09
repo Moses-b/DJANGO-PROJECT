9f82f94f670f14835c4a6e785169d81ff329dbba
ZIMAY Moses Z <81933166+Moses-b@users.noreply.github.com>
2025-08-09 01:11:05 +0000

    Good

--- src/Myportofolio/settings.py
+++ src/Myportofolio/settings.py
@@ -16,10 +16,6 @@ from pathlib import Path
 import environ
 import dj_database_url
 
-
-
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -85,13 +81,14 @@ WSGI_APPLICATION = 'Myportofolio.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
+"""
 DATABASE = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
 
 DATABASES = {
     'default': dj_database_url.parse(environ('DATABASE_URL'))
