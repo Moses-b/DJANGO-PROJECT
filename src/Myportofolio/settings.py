7efc1a0d9dfdfefc735e4b5bddca91db823e0668
ZIMAY Moses Z <81933166+Moses-b@users.noreply.github.com>
2025-08-09 02:15:08 +0000

    Full

--- src/Myportofolio/settings.py
+++ src/Myportofolio/settings.py
@@ -19,21 +19,21 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env()
-environ.Env.read_env()
 
-
+env = environ.Env(
+    # Définissez les valeurs par défaut
+    DEBUG=(bool, False) 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
@@ -91,9 +91,8 @@ DATABASE = {
 """
 
 DATABASES = {
-    'default': dj_database_url.parse(environ('DATABASE_URL'))
-    }
-
+    'default': env.db()
+}
 
 
 # Password validation
