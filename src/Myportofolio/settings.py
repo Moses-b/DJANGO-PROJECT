c8ad9091fe7328a1bb2090527736e6cb20ef168c
ZIMAY Moses Z <81933166+Moses-b@users.noreply.github.com>
2025-08-10 10:19:52 +0000

    ok

--- src/Myportofolio/settings.py
+++ src/Myportofolio/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'yqs=4le%!%3qvn)ysb3j+jp5o8kvieziu03g_jntkj1%v^@bfw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dpg-d2bbf2p5pdvs73cjkrjg-a']  # Update this with your actual domain in production
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost']) # Update this with your actual domain in production
 # Application definition
 
 INSTALLED_APPS = [
